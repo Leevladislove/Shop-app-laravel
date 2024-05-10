91f90e6f89ac9ae4fa54b8360b8fdf24462458c8
Lee <leevladislove@gmail.com>
2024-05-10 21:31:00 +0500

    Create products/2

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Good;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $goods = Good::query()->get();
+        return view('home', compact('goods'));
     }
 }
